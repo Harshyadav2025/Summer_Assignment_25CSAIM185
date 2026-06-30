da66d01e810155c68d6380e438434414718d2db5
Harshyadav2025 <harsh.yadav2025@glbajajgroup.org>
2026-06-30 19:30:20 +0530

    Add files via upload

---
+++ DAY-13/P-51.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main() 
+{
+    int n, i;
+
+  
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    int largest = arr[0];
+    int smallest = arr[0];
+
+    for(i = 1; i < n; i++) 
+    {
+        if(arr[i] > largest)
+        {
+            largest = arr[i]; 
+        }
+        if(arr[i] < smallest)
+        {
+            smallest = arr[i]; 
+        }
+    }
+
+    printf("\nLargest element = %d", largest);
+    printf("\nSmallest element = %d\n", smallest);
+
+    return 0;
+}
